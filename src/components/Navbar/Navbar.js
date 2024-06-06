0842de3cbe8ec40d73647a6fedaa3f4eb705c262
Allan Andrade de Jesus <allan.aj1993@gmail.com>
2024-06-06 16:06:12 -0300

    primeiro commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import styles from './Navbar.module.scss'
+import {ReactComponent as Logo }from "../../assets/logo.svg"
+import classNames from 'classnames'
+import {RiShoppingCart2Line, RiShoppingCartFill} from 'react-icons/ri';
+import Busca from 'components/Busca/Busca';
+
+const iconeProps = {
+    color: 'white',
+    size: 24
+  }
+
+export default function Navbar(){
+    return (
+        <nav className={styles.nav}>
+            <Logo className={styles.logo} />
+            <div className={styles.links}>
+                <a href="/" className={classNames(styles.link, {
+                    [styles.selected]: window.location.pathname === '/'
+                })}>
+                    Pagina inicial
+                </a>
+            </div>
+
+            <div className={styles.busca}>
+                <Busca/>
+            </div>
+            <div className={styles.icones}>
+                <a href='/carrinho'>
+                    {window.location.pathname === '/carrinho' ? <RiShoppingCartFill {...iconeProps} /> : <RiShoppingCart2Line {...iconeProps} />}
+                </a>
+            </div>
+        </nav>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import Navbar from './components/Navbar/Navbar';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Navbar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
+
